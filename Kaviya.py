8bd1f276d7a66579067cf63454c40beb1e088b99
kaviyabharathi18 <117108698+kaviyabharathi18@users.noreply.github.com>
2022-10-31 23:05:42 +0530

    Add files via upload

---
+++ Kaviya.py
@@ -0,0 +1,25 @@
+name=(input("enter your name:"))
+classname=(input("enter your class :"))
+section=(input("enter section:"))
+schoolname=(input("enter your schoolname:"))
+classteachername=(input("enter your class teacher name:"))
+date=(input("enter the date:"))
+sub=(input("enter subject:"))
+dateofleave=(input("enter date of leave:"))
+reason=(input("enter reason:"))
+address=(input("enter your address:"))
+
+newstring =f'''To {classteachername} ,
+{schoolname}
+{address}
+
+{date}
+
+{sub}
+
+Dear sir,
+
+{reason}
+
+Thanking you
+
